0a2edcf3fc4794ca8ada694eab0bd21f222d41d6
nitram509 <maki@bitkings.de>
2012-07-26 00:36:47 +0200

    initial commit

---
+++ data.js
@@ -0,0 +1,14 @@
+var viz_data = {
+	'label' : [ 'ToDo', 'WiP', 'Done'],
+	'values' : [ {
+		'label' : 'ToDo',
+		'values' : [ 20, 40, 15 ]
+	}, {
+		'label' : 'WiP',
+		'values' : [ 30, 10, 45 ]
+	}, {
+		'label' : 'Done',
+		'values' : [ 26, 40, 25 ]
+	} ]
+
+};
