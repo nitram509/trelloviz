829f2a84c18e732391a1a49727beb383b26a63ef
nitram509 <maki@bitkings.de>
2012-07-27 21:33:26 +0200

    added sample action data

---
+++ sample_test_board_actions.js
@@ -0,0 +1,301 @@
+[{
+		"id" : "5012e5fad0aacbde765faaea",
+		"idMemberCreator" : "500da53e30a2363615013888",
+		"data" : {
+			"old" : {
+				"closed" : false
+			},
+			"board" : {
+				"name" : "Tests",
+				"id" : "500daadf637e1efe2a2348f9"
+			},
+			"card" : {
+				"closed" : true,
+				"name" : "done2",
+				"idShort" : 6,
+				"id" : "500daafd637e1efe2a234efd"
+			}
+		},
+		"type" : "updateCard",
+		"date" : "2012-07-27T19:03:22.242Z",
+		"memberCreator" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		}
+	}, {
+		"id" : "500f0e6fe3239f0d172998d7",
+		"idMemberCreator" : "500da53e30a2363615013888",
+		"data" : {
+			"idMember" : "500da53e30a2363615013888",
+			"board" : {
+				"name" : "Tests",
+				"id" : "500daadf637e1efe2a2348f9"
+			},
+			"card" : {
+				"name" : "todo1",
+				"idShort" : 1,
+				"id" : "500daae8637e1efe2a234b76"
+			}
+		},
+		"type" : "addMemberToCard",
+		"date" : "2012-07-24T21:06:55.625Z",
+		"member" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		},
+		"memberCreator" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		}
+	}, {
+		"id" : "500f0e42e3239f0d172990a2",
+		"idMemberCreator" : "500da53e30a2363615013888",
+		"data" : {
+			"text" : "noch ein Kommentar",
+			"board" : {
+				"name" : "Tests",
+				"id" : "500daadf637e1efe2a2348f9"
+			},
+			"card" : {
+				"name" : "todo1",
+				"idShort" : 1,
+				"id" : "500daae8637e1efe2a234b76"
+			}
+		},
+		"type" : "commentCard",
+		"date" : "2012-07-24T21:06:10.504Z",
+		"memberCreator" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		}
+	}, {
+		"id" : "500f0dddf3027a081832bb91",
+		"idMemberCreator" : "500da53e30a2363615013888",
+		"data" : {
+			"text" : "test a comment on doin 2",
+			"board" : {
+				"name" : "Tests",
+				"id" : "500daadf637e1efe2a2348f9"
+			},
+			"card" : {
+				"name" : "doin2",
+				"idShort" : 4,
+				"id" : "500daaf1637e1efe2a234ce7"
+			}
+		},
+		"type" : "commentCard",
+		"date" : "2012-07-24T21:04:29.375Z",
+		"memberCreator" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		}
+	}, {
+		"id" : "500daafd637e1efe2a234eff",
+		"idMemberCreator" : "500da53e30a2363615013888",
+		"data" : {
+			"list" : {
+				"name" : "Done",
+				"id" : "500daadf637e1efe2a2348fc"
+			},
+			"board" : {
+				"name" : "Tests",
+				"id" : "500daadf637e1efe2a2348f9"
+			},
+			"card" : {
+				"name" : "done2",
+				"idShort" : 6,
+				"id" : "500daafd637e1efe2a234efd"
+			}
+		},
+		"type" : "createCard",
+		"date" : "2012-07-23T19:50:21.117Z",
+		"memberCreator" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		}
+	}, {
+		"id" : "500daafb637e1efe2a234eb9",
+		"idMemberCreator" : "500da53e30a2363615013888",
+		"data" : {
+			"list" : {
+				"name" : "Done",
+				"id" : "500daadf637e1efe2a2348fc"
+			},
+			"board" : {
+				"name" : "Tests",
+				"id" : "500daadf637e1efe2a2348f9"
+			},
+			"card" : {
+				"name" : "done1",
+				"idShort" : 5,
+				"id" : "500daafb637e1efe2a234eb7"
+			}
+		},
+		"type" : "createCard",
+		"date" : "2012-07-23T19:50:19.366Z",
+		"memberCreator" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		}
+	}, {
+		"id" : "500daaf6637e1efe2a234d78",
+		"idMemberCreator" : "500da53e30a2363615013888",
+		"data" : {
+			"old" : {
+				"name" : "goin2"
+			},
+			"board" : {
+				"name" : "Tests",
+				"id" : "500daadf637e1efe2a2348f9"
+			},
+			"card" : {
+				"name" : "doin2",
+				"idShort" : 4,
+				"id" : "500daaf1637e1efe2a234ce7"
+			}
+		},
+		"type" : "updateCard",
+		"date" : "2012-07-23T19:50:14.557Z",
+		"memberCreator" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		}
+	}, {
+		"id" : "500daaf1637e1efe2a234ce9",
+		"idMemberCreator" : "500da53e30a2363615013888",
+		"data" : {
+			"list" : {
+				"name" : "Doing",
+				"id" : "500daadf637e1efe2a2348fb"
+			},
+			"board" : {
+				"name" : "Tests",
+				"id" : "500daadf637e1efe2a2348f9"
+			},
+			"card" : {
+				"name" : "goin2",
+				"idShort" : 4,
+				"id" : "500daaf1637e1efe2a234ce7"
+			}
+		},
+		"type" : "createCard",
+		"date" : "2012-07-23T19:50:09.593Z",
+		"memberCreator" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		}
+	}, {
+		"id" : "500daaee637e1efe2a234c5e",
+		"idMemberCreator" : "500da53e30a2363615013888",
+		"data" : {
+			"list" : {
+				"name" : "Doing",
+				"id" : "500daadf637e1efe2a2348fb"
+			},
+			"board" : {
+				"name" : "Tests",
+				"id" : "500daadf637e1efe2a2348f9"
+			},
+			"card" : {
+				"name" : "doin1",
+				"idShort" : 3,
+				"id" : "500daaee637e1efe2a234c5c"
+			}
+		},
+		"type" : "createCard",
+		"date" : "2012-07-23T19:50:06.813Z",
+		"memberCreator" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		}
+	}, {
+		"id" : "500daaea637e1efe2a234bc6",
+		"idMemberCreator" : "500da53e30a2363615013888",
+		"data" : {
+			"list" : {
+				"name" : "To Do",
+				"id" : "500daadf637e1efe2a2348fa"
+			},
+			"board" : {
+				"name" : "Tests",
+				"id" : "500daadf637e1efe2a2348f9"
+			},
+			"card" : {
+				"name" : "todo2",
+				"idShort" : 2,
+				"id" : "500daaea637e1efe2a234bc4"
+			}
+		},
+		"type" : "createCard",
+		"date" : "2012-07-23T19:50:02.868Z",
+		"memberCreator" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		}
+	}, {
+		"id" : "500daae8637e1efe2a234b78",
+		"idMemberCreator" : "500da53e30a2363615013888",
+		"data" : {
+			"list" : {
+				"name" : "To Do",
+				"id" : "500daadf637e1efe2a2348fa"
+			},
+			"board" : {
+				"name" : "Tests",
+				"id" : "500daadf637e1efe2a2348f9"
+			},
+			"card" : {
+				"name" : "todo1",
+				"idShort" : 1,
+				"id" : "500daae8637e1efe2a234b76"
+			}
+		},
+		"type" : "createCard",
+		"date" : "2012-07-23T19:50:00.420Z",
+		"memberCreator" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		}
+	}, {
+		"id" : "500daadf637e1efe2a2348ff",
+		"idMemberCreator" : "500da53e30a2363615013888",
+		"data" : {
+			"board" : {
+				"name" : "Tests",
+				"id" : "500daadf637e1efe2a2348f9"
+			}
+		},
+		"type" : "createBoard",
+		"date" : "2012-07-23T19:49:51.976Z",
+		"memberCreator" : {
+			"id" : "500da53e30a2363615013888",
+			"fullName" : "Martin Kirst",
+			"initials" : "MK",
+			"username" : "martinkirst1"
+		}
+	}
+]
