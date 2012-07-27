8a5a96d26479e5b370e88966fcd72e4516b13dcb
nitram509 <maki@bitkings.de>
2012-07-28 01:02:25 +0200

    first implementation of calculating list changes

--- trelloviz-app.js
+++ trelloviz-app.js
@@ -1,3 +1,79 @@
+var computeVizData = function(actions) {
+    var vizTimestamps = [];
+    var vizPlan = [];
+    var vizWiP = [];
+    var vizDone = [];
+
+    var idPlan = "500daadf637e1efe2a2348fa";
+    var idWiP = "500daadf637e1efe2a2348fb";
+    var idDone = "500daadf637e1efe2a2348fc";
+
+    var counterPerList = {};
+    var cardToListMap = {};
+
+    counterPerList[idPlan] = 0;
+    counterPerList[idWiP] = 0;
+    counterPerList[idDone] = 0;
+
+    data.sort(function (a, b) {
+        var unixtimestamp_a = Date.parse(a.date);
+        var unixtimestamp_b = Date.parse(b.date);
+        if (unixtimestamp_a < unixtimestamp_b) return -1;
+        if (unixtimestamp_a > unixtimestamp_b) return 1;
+        return 0;
+    });
+
+    $.each(data, function (idx, trelloAction) {
+        console.info(trelloAction.date);
+        var unixtimestamp = trelloAction.date;// Date.parse(trelloAction.date);
+        console.info(new Date(unixtimestamp));
+
+        if (trelloAction.type == 'createCard') {
+            counterPerList[trelloAction.data.list.id]++;
+            cardToListMap[trelloAction.data.card.id] = trelloAction.data.list.id;
+        } else if (trelloAction.type == 'updateCard') {
+            if (trelloAction.data.card.closed == true && trelloAction.data.old.closed == false) {
+                counterPerList[cardToListMap[trelloAction.data.card.id]]--;
+            }
+            if (typeof trelloAction.data.listAfter != "undefined"
+                && trelloAction.data.listBefore != "undefined"
+                && trelloAction.data.listAfter.id != trelloAction.data.listBefore.id) {
+                // verschoben
+                counterPerList[trelloAction.data.listAfter.id]++;
+                counterPerList[trelloAction.data.listBefore.id]--;
+            }
+        } else {
+            console.info(trelloAction.type);
+        }
+
+        vizTimestamps.push(unixtimestamp);
+        vizPlan.push(counterPerList[idPlan]);
+        vizWiP.push(counterPerList[idWiP]);
+        vizDone.push(counterPerList[idDone]);
+
+    });
+
+    var viz_data = {
+        'label':vizTimestamps,
+        'values':[
+            {
+                'label':'ToDo',
+                'values':vizPlan
+            },
+            {
+                'label':'WiP',
+                'values':vizWiP
+            },
+            {
+                'label':'Done',
+                'values':vizDone
+            }
+        ]
+
+    };
+    return viz_data;
+}
+
 var showCards = function(cards) {
 	var $uiwidget = $('<div class="ui-widget">').appendTo("#output");
     $('<p>').text('Your Cards: ').appendTo($uiwidget);
@@ -38,7 +114,7 @@ var showLists = function(lists) {
 };
 
 function onShowActionForBoard(boardId) {
-	Trello.get("boards/"+boardId+"/actions", {fields:"data,type,date"},  showActions);
+	Trello.get("boards/"+boardId+"/actions", { /* fields:"data,type,date" */ limit:"1000"},  computeVizData);
 }
 
 function onBoardSelected(boardId) {
