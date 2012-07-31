679a3ea5ca74d6086cd8f2131b5d54e4d2ed2940
nitram509 <maki@bitkings.de>
2012-07-31 22:20:46 +0200

    refactored Trelloviz.js - currently broken :-/ Added settings UI window

---
+++ js/Trelloviz.js
@@ -0,0 +1,292 @@
+/**
+ * Trelloviz
+ * Copyright 2012 Martin W. Kirst
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+function Trelloviz () {
+    var trellovizData = new TrellovizData();
+}
+
+
+Trelloviz.prototype.showCards = function (cards) {
+    var $uiwidget = $('<div class="ui-widget">').appendTo("#output");
+    $('<p>').text('Your Cards: ').appendTo($uiwidget);
+    $.each(cards, function (idx, card) {
+        $('<p style="padding-left: 2em;">').text(card.name + ' (' + card.id + ')').appendTo($uiwidget);
+    });
+};
+
+
+Trelloviz.prototype.showActions = function (actions) {
+    var $uiwidget = $('<div class="ui-widget">').appendTo("#output");
+    $('<p>').text('All Actions: ').appendTo($uiwidget);
+    var $ul = $('<ul>').appendTo($uiwidget);
+    $.each(actions, function (idx, action) {
+        $('<li>').text(action.type + ' (' + action.date + ')').appendTo($ul);
+    });
+};
+
+
+Trelloviz.prototype.onListSelected = function (listId) {
+    Trello.get("lists/" + listId + "/cards", {}, me.showCards);
+}
+
+
+Trelloviz.prototype.showLists = function (lists) {
+    var $uiwidget = $('<div class="ui-widget">').appendTo("#output");
+    $('<label>').text('Your Lists: ').appendTo($uiwidget);
+    var $select = $('<select id="comboboxLists">').appendTo($uiwidget);
+
+    $("#comboboxLists").combobox({
+            selected:function (event, ui) {
+                var listid = ui.item.value;
+                me.onListSelected(listid);
+            }
+        }
+    );
+
+    $.each(lists, function (idx, list) {
+        $('<option value=' + list.id + '>').text(list.name).appendTo($select);
+    });
+};
+
+
+Trelloviz.prototype.computeAndShow = function(data) {
+    var computed = this.trellovizData.computeVizData();
+    this.showGraphic(computed);
+};
+
+
+Trelloviz.prototype.onShowActionForBoard = function (boardId) {
+    Trello.get("boards/" + boardId + "/actions", { /* fields:"data,type,date" */ limit:"1000"}, me.computeAndShow);
+}
+
+
+Trelloviz.prototype.onBoardSelected = function (boardId) {
+    var $uiwidget = $('<div class="ui-widget">').appendTo("#output");
+    var $btn = $('<button>').text('Show Actions').appendTo($uiwidget);
+    $btn.button();
+    $btn.click(function () {
+        me.onShowActionForBoard(boardId);
+    });
+
+    Trello.get("boards/" + boardId + "/lists", { cards:"all" }, me.showLists);
+}
+
+
+Trelloviz.prototype.showBoards = function (boards) {
+    $("#output").empty();
+
+    var $uiwidget = $('<div class="ui-widget">').appendTo("#output");
+    $('<label>').text('Your boards: ').appendTo($uiwidget);
+    var $select = $('<select id="combobox">').appendTo($uiwidget);
+
+    $("#combobox").combobox({
+            selected:function (event, ui) {
+                var boardid = ui.item.value;
+                me.onBoardSelected(boardid);
+            }
+        }
+    );
+
+    $.each(boards, function (idx, board) {
+        $('<option value=' + board.id + '>').text(board.name).appendTo($select);
+    });
+};
+
+
+Trelloviz.prototype.onAuthorize = function () {
+    me.updateLoggedIn();
+
+    Trello.members.get("me", function (member) {
+        $("#fullName").text(member.fullName);
+    });
+
+    Trello.get("members/me/boards", {}, me.showBoards);
+};
+
+
+Trelloviz.prototype.updateLoggedIn = function () {
+    var isLoggedIn = (typeof Trello != "undefined") && Trello.authorized();
+    $("#connectButton").toggle(!isLoggedIn);
+    $("#disconnectButton, #loggedin").toggle(isLoggedIn);
+};
+
+
+Trelloviz.prototype.logout = function () {
+    Trello.deauthorize();
+    me.updateLoggedIn();
+};
+
+
+Trelloviz.prototype.trelloLogin = function (data, textStatus, jqxhr) {
+    var successCallback = this.onAuthorize;
+    Trello.authorize({
+        interactive:true,
+        type:"popup",
+        success:successCallback,
+        expiration:"1hour",
+        scope:{ read:true, write:false }
+    });
+};
+
+
+Trelloviz.prototype.showSettings = function() {
+
+
+        var name = $( "#name" ),
+            email = $( "#email" ),
+            password = $( "#password" ),
+            allFields = $( [] ).add( name ).add( email ).add( password ),
+            tips = $( ".validateTips" );
+
+        function updateTips( t ) {
+            tips
+                .text( t )
+                .addClass( "ui-state-highlight" );
+            setTimeout(function() {
+                tips.removeClass( "ui-state-highlight", 1500 );
+            }, 500 );
+        }
+
+        function checkLength( o, n, min, max ) {
+            if ( o.val().length > max || o.val().length < min ) {
+                o.addClass( "ui-state-error" );
+                updateTips( "Length of " + n + " must be between " +
+                    min + " and " + max + "." );
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        function checkRegexp( o, regexp, n ) {
+            if ( !( regexp.test( o.val() ) ) ) {
+                o.addClass( "ui-state-error" );
+                updateTips( n );
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        $( "#dialog-form" ).dialog({
+            autoOpen: false,
+            height: 300,
+            width: 350,
+            modal: true,
+            buttons: {
+                "Create an account": function() {
+                    var bValid = true;
+                    allFields.removeClass( "ui-state-error" );
+
+                    bValid = bValid && checkLength( name, "username", 3, 16 );
+                    bValid = bValid && checkLength( email, "email", 6, 80 );
+                    bValid = bValid && checkLength( password, "password", 5, 16 );
+
+                    bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
+                    // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
+                    bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
+                    bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+
+                    if ( bValid ) {
+                        $( "#users tbody" ).append( "<tr>" +
+                            "<td>" + name.val() + "</td>" +
+                            "<td>" + email.val() + "</td>" +
+                            "<td>" + password.val() + "</td>" +
+                            "</tr>" );
+                        $( this ).dialog( "close" );
+                    }
+                },
+                Cancel: function() {
+                    $( this ).dialog( "close" );
+                }
+            },
+            close: function() {
+                allFields.val( "" ).removeClass( "ui-state-error" );
+            }
+        });
+
+        $( "#create-user" )
+            .button()
+            .click(function() {
+                $( "#dialog-form" ).dialog( "open" );
+            });
+
+}
+
+
+Trelloviz.prototype.bindActions = function () {
+    var callback = this.trelloLogin;
+
+    $( "#dialog-form" ).dialog({ autoOpen: false });
+
+    $("#settingsButton").button();
+    $("#settingsButton").click(function(){
+        $( "#dialog-form" ).dialog( "open" );
+    });
+
+    $("#connectButton").button();
+    $("#connectButton").click(function () {
+        jQuery.getScript('https://api.trello.com/1/client.js?key=' + TRELLO_API_KEY, callback);
+    });
+
+    $("#disconnectButton").button();
+    $("#disconnectButton").click(this.logout);
+    this.updateLoggedIn();
+};
+
+
+Trelloviz.prototype.showGraphic = function (viz_data) {
+    var areaChart = new $jit.AreaChart({
+        //id of the visualization container
+        injectInto:'viz_canvas',
+        //add animations
+        animate:true,
+        Margin:{ top:5, left:5, right:5, bottom:5 },
+        labelOffset:10,
+        showAggregates:true,
+        showLabels:true,
+        type:'stacked:gradient',
+        //label styling
+        Label:{
+            type:'Native', //can be 'Native' or 'HTML'
+            size:13,
+            family:'monospace',
+            color:'silver'
+        },
+        //enable tips
+        Tips:{
+            enable:true,
+            onShow:function (tip, elem) {
+                var tt = document.createElement("span");
+                tt.className = "tooltip"
+                tt.innerHTML = "" + elem.name + " " + elem.value;
+                while (tip.hasChildNodes()) tip.removeChild(tip.firstChild);
+                tip.appendChild(tt);
+            }
+        },
+        //add left and right click handlers
+        filterOnClick:false,
+        restoreOnRightClick:true
+    });
+    areaChart.loadJSON(viz_data);
+};
+
---
+++ js/TrellovizData.js
@@ -0,0 +1,89 @@
+/**
+ * Trelloviz
+ * Copyright 2012 Martin W. Kirst
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+TrellovizData = function() {
+
+}
+
+TrellovizData.prototype.computeVizData = function (trelloData) {
+    var vizTimestamps = [];
+    var vizPlan = [];
+    var vizWiP = [];
+    var vizDone = [];
+
+    var idPlan = "500daadf637e1efe2a2348fa";
+    var idWiP = "500daadf637e1efe2a2348fb";
+    var idDone = "500daadf637e1efe2a2348fc";
+
+    var counterPerList = {};
+    var cardToListMap = {};
+
+    counterPerList[idPlan] = 0;
+    counterPerList[idWiP] = 0;
+    counterPerList[idDone] = 0;
+
+    trelloData.sort(function (a, b) {
+        var unixtimestamp_a = Date.parse(a.date);
+        var unixtimestamp_b = Date.parse(b.date);
+        if (unixtimestamp_a < unixtimestamp_b) return -1;
+        if (unixtimestamp_a > unixtimestamp_b) return 1;
+        return 0;
+    });
+
+    var vizDataForJit = {
+        'label':['ToDo', 'WiP', 'Done'],
+        'values':[]
+    };
+
+    $.each(trelloData, function (idx, trelloAction) {
+        console.info(trelloAction.date);
+        var unixtimestamp = trelloAction.date;// Date.parse(trelloAction.date);
+        console.info(new Date(unixtimestamp));
+
+        if (trelloAction.type == 'createCard') {
+            counterPerList[trelloAction.data.list.id]++;
+            cardToListMap[trelloAction.data.card.id] = trelloAction.data.list.id;
+        } else if (trelloAction.type == 'updateCard') {
+            if (trelloAction.data.card.closed == true && trelloAction.data.old.closed == false) {
+                counterPerList[cardToListMap[trelloAction.data.card.id]]--;
+            }
+            if (typeof trelloAction.data.listAfter != "undefined"
+                && trelloAction.data.listBefore != "undefined"
+                && trelloAction.data.listAfter.id != trelloAction.data.listBefore.id) {
+                // verschoben
+                counterPerList[trelloAction.data.listAfter.id]++;
+                counterPerList[trelloAction.data.listBefore.id]--;
+            }
+        } else {
+            console.info(trelloAction.type);
+        }
+
+        vizDataForJit.values.push(
+            {
+                'label':unixtimestamp,
+                'values':[counterPerList[idPlan], counterPerList[idWiP],counterPerList[idDone]]
+            }
+        );
+    });
+
+    return vizDataForJit;
+};
