39d824a46a7645f5461ca08ec41618eda8db66e3
nitram509 <maki@bitkings.de>
2012-08-21 00:49:24 +0200

    added color picker - updating viewModel doesn't work yet :-/

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@ rm -rf target
 mkdir target
 
 cp -ar src/* target/
-java -jar compiler.jar --js src/js/Trelloviz.js     --js_output_file target/js/Trelloviz.js
+java -jar compiler.jar --js src/js/Trelloviz.js --js src/js/color_picker_knockout_binding.js  --js_output_file target/js/Trelloviz.js
 java -jar compiler.jar --js src/js/TrellovizData.js --js_output_file target/js/TrellovizData.js
 
 java -jar compiler.jar --js src/assets/js/bootstrap-dropdown.js --js_output_file target/assets/js/bootstrap-dropdown.js
 java -jar compiler.jar --js src/assets/js/jit.js                --js_output_file target/assets/js/jit.js
 java -jar compiler.jar --js src/assets/js/jquery.js             --js_output_file target/assets/js/jquery.js
 java -jar compiler.jar --js src/assets/js/knockout.js           --js_output_file target/assets/js/knockout.js
+java -jar compiler.jar --js src/assets/js/jquery.colorPicker.js           --js_output_file target/assets/js/jquery.colorPicker.js
