a8089caf4369f4bd31af6bb0b8882e383fc08ceb
nitram509 <maki@bitkings.de>
2013-05-25 19:23:47 +0200

    inserted CfdController.js with cfd-panel.html view

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -6,6 +6,7 @@ var Trelloviz = angular.module('Trelloviz', []);
 Trelloviz.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.
      when('/config', {templateUrl: 'partials/config.html', controller: 'Trelloviz.ConfigController'}).
+     when('/cfd', {templateUrl: 'partials/cfd-panel.html', controller: 'Trelloviz.CfdController'}).
      when('/', {templateUrl: 'partials/welcome.html'}).
      otherwise({redirectTo: '/'});
 }]);
